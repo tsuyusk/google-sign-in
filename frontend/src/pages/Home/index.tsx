a6b765a95ca23527aae9b41669ca053236db0429
Tsuy <rafaeldesenhos99@gmail.com>
2023-01-28 12:03:49 -0300

    Feat: Insert obtained user data from google in Home Page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import { useCallback } from "react";
-import { useDispatch } from "react-redux"
-import { signOff } from "../../redux/features/user/user-slice";
+import { useDispatch, useSelector } from "react-redux"
+import { getUser, signOff } from "../../redux/features/user/user-slice";
 
 export function Home() {
   const dispatch = useDispatch();
+  const user = useSelector(getUser);
 
   const handleSignOff = useCallback(() => {
     dispatch(
@@ -13,7 +14,8 @@ export function Home() {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center bg-zinc-800 text-white">
-      <h1 className="text-xl font-bold">Home</h1>
+      <h1 className="text-xl font-bold">Welcome, {user?.displayName}</h1>
+      <p className="text-gray-400">Your e-mail is {user?.email}</p>
 
       <button
         className="bg-blue-500 text-white py-2 px-4 rounded-md transition-all duration-150 cursor-pointer hover:bg-blue-700"
